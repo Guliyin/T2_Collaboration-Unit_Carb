b67b406e471823c417f955b06e62ce2e2e144a1c
Guliyin <guliyin0115@163.com>
2024-02-05 03:03:19 +0000

    血条特效完成

--- Assets/Scripts/HealthBar_Boss.cs
+++ Assets/Scripts/HealthBar_Boss.cs
@@ -2,58 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
-public class HealthBar : MonoBehaviour
+public class HealthBar_Boss : MonoBehaviour
 {
-    const float DAMAGED_HEALTH_SHRINK_TIMER_MAX = 0.5f;
-    float damagedHealthShrinkTimer;
+    const float BAR_WIDTH = 1000f;
 
     Image healthBar;
-    Image damageBar;
+    Transform damagedBarTemplate;
     HealthSystem healthSystem;
 
     private void Awake()
     {
         healthBar = transform.Find("Fill").GetComponent<Image>();
-        damageBar = transform.Find("DamageBar").GetComponent<Image>();
+        damagedBarTemplate = transform.Find("damagedBarTemplate");
     }
 
     void Start()
     {
         healthSystem = new HealthSystem(1000);
         SetHealth(healthSystem.GetHealthNormalized());
-        damagedHealthShrinkTimer = DAMAGED_HEALTH_SHRINK_TIMER_MAX;
 
         healthSystem.OnDamaged += HealthSystem_OnDamaged;
     }
     void Update()
     {
 
-        damagedHealthShrinkTimer -= Time.deltaTime;
-        if (damagedHealthShrinkTimer < 0)
-        {
-            if (healthBar.fillAmount < damageBar.fillAmount)
-            {
-                damageBar.fillAmount -= 0.5f * Time.deltaTime;
-            }
-        }
-
         if (Input.GetKeyDown(KeyCode.P))
         {
-            print("?");
             healthSystem.Damage(10);
         }
     }
 
     void HealthSystem_OnDamaged(object sender, System.EventArgs e)
     {
-        damagedHealthShrinkTimer = DAMAGED_HEALTH_SHRINK_TIMER_MAX;
+        float beforeDamaged = healthBar.fillAmount;
         SetHealth(healthSystem.GetHealthNormalized());
+        Transform go = Instantiate(damagedBarTemplate, transform);
+        go.gameObject.SetActive(true);
+        RectTransform damagedBar = go.GetComponent<RectTransform>();
+        damagedBar.anchoredPosition = new Vector2(healthBar.fillAmount * BAR_WIDTH, damagedBar.anchoredPosition.y);
+        damagedBar.GetComponent<Image>().fillAmount = beforeDamaged - healthBar.fillAmount;
+
+        damagedBar.DOAnchorPosY(-20, 0.3f);
+        damagedBar.GetComponent<Image>().DOFade(0, 0.3f);
+        Destroy(damagedBar.gameObject, 0.4f);
     }
 
     void SetHealth(float healthNormalized)
     {
         healthBar.fillAmount = healthNormalized;
     }
-
 }
